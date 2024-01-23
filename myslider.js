91178fd63281089c00eb8e66260d821a00c71b0b
Ramin Farhadi <ra680091@CMN9J7HFG4MN.local>
2024-01-23 11:44:47 -0500

    Draft v1.0

--- myslider.js
+++ myslider.js
@@ -23,37 +23,74 @@ const faculties = [
     },
 ]
 
-// Controllers
-const sliderContext = document.querySelector('#slider-context')
+// Variables
+const slides = []
+let counter = 0;
+let pagination = '';
+let circleBehavior = '';
 
-slides = []
+// Controllers
+const sliderContext = document.querySelector('#slider-context');
+const sliderPagination = document.querySelector('#sliderPagination');
+const textBox = document.querySelector('.sliderTextBox');
+
 faculties.forEach(element => {
     const slide = `
-    <div class="col-xs-12 col-lg-6 text-center text-light h-100">
-        <img class="" src="${element.imageAddress}" alt="${element.title}"  />
-    </div>
-    <div  class="col-xs-0 col-lg-6 text-light sliderTextBox">
-        <p class="employeeName display-4 font-weight-bold">${element.title}</p>
-        <p class="employeeTitle h3" >${element.subtitle}</p>
-        <br>
-        <p class="employeeDesc h5">${element.des}</p>
-    </div>`;
+        <div class="col-xs-12 col-lg-6 text-center text-light h-100">
+            <img class="" src="${element.imageAddress}" alt="${element.title}"  />
+        </div>
+        <div  class="col-xs-0 col-lg-6 text-light sliderTextBox">
+            <p class="employeeName display-4 font-weight-bold">${element.title}</p>
+            <p class="employeeTitle h3" >${element.subtitle}</p>
+            <br>
+            <p class="employeeDesc h5">${element.des}</p>
+        </div>`;
 
     slides.push(slide);
 })
-
-counter = 0;
 sliderContext.innerHTML = slides[counter]
-console.log(slides)
-// Events
 
+// Events
+// Pagination Function 
+const paginationFunc = (counter) => {
+    pagination = '';
+    slides.forEach((element, index) => {
+            index === counter ? circleBehavior = 'yellow' : circleBehavior = 'white';
+            pagination += `<svg style="display:block" height="50" width="50">
+            <circle cx="25" cy="25" r="5" stroke="black" stroke-width="1" fill="${circleBehavior}" />
+            </svg>`
+    })
+    sliderPagination.innerHTML= pagination
+}
+paginationFunc(counter);
+
+// Counter increment function
+function incrementCounter() {
+    counter++;
+    if (counter >= slides.length) {
+        counter = 0   
+    }
+    sliderContext.innerHTML = slides[counter];
+    paginationFunc(counter)
+  }
+  
+  setInterval(incrementCounter, 7000);
+
+// scrolling Function
 scrollFunc = (event) => {
-    event.deltaY > 0 ? counter++ : event.deltaY < 0 ? counter-- : "";
+    if(event.deltaY > 0){ 
+        counter++; 
+    } else if (event.deltaY < 0) { 
+        counter--;
+    }
     if (counter >= slides.length) {
         counter = 0
+        
     }else if (counter < 0) {
         counter = slides.length - 1;
     }
     sliderContext.innerHTML = slides[counter];
-    
+
+    paginationFunc(counter)
+    console.log('counter ' + counter)
 }
